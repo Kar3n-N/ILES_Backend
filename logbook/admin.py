855c5ea138d826371b0cdf44374067fd8677215c
Nakibuuka Karen <k41444105@gmail.com>
2026-03-16 15:05:59 +0300

    writing apps in models.py

--- logbook/admin.py
+++ logbook/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Logbook
+
+admin.site.register(Logbook)
 
 # Register your models here.
--- logbook/models.py
+++ logbook/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.conf import settings
+from placements.models import InternshipPlacement
 
-# Create your models here.
+class Logbook(models.Model):
+
+    STATUS = [
+        ('draft', 'Draft'),
+        ('pending', 'Pending'),
+        ('approved', 'Approved'),
+        ('reviewed', 'Reviewed'),
+        ('submitted', 'Submitted'),                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+    ]
+
+    week_number = models.PositiveIntegerField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    activities = models.CharField()
+    status = models.CharField(max_length=20, 
+        choices = STATUS, 
+        default = 'draft'
+    )
+    
+    student = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE,
+        related_name='Weekly_logs'
+    )
+
+    placement = models.ForeignKey(
+        InternshipPlacement,
+        on_delete=models.CASCADE,
+        related_name='Weekly_logs'
+    )
+
+    submitted_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('student','week_number','placement')
+
+    def __str__(self):
+        return f"Week {self.week_number} - {self.student.username} [{self.status}]"
