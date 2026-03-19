54b222834a085fddb5480e592c9953d5d157d58d
Sabano Zubedah Angel <sabanozubedah@gmail.com>
2026-03-19 16:49:56 +0300

    Creating Placements Model

--- placements/admin.py
+++ placements/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import InternshipPlacement
 
+admin.site.register(InternshipPlacement)
 # Register your models here.
--- placements/models.py
+++ placements/models.py
@@ -1,6 +1,26 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 class InternshipPlacement(models.Model):
+    student = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='placements',
+        null=True
+    )
+    company_name = models.CharField(max_length=200,null=True)
+    start_date = models.DateTimeField(default=timezone.now)
+    end_date = models.DateTimeField(default=timezone.now)
+    supervisor = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='supervised_placements'
+    )
+
     def __str__(self):
-        return "run"
+        return f"{self.student} @ {self.company_name}" 
+
+
 
