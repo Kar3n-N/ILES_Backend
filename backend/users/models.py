d2535b30cde9cab4af56757db3ecc5fe033ea717
Matthew Mumbere <matthewmumbere@gmail.com>
2026-03-26 13:04:05 +0300

    Adding the frontend

---
+++ backend/users/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    ROLES = [
+        ("student", "Student Intern"),
+        ("workplace_supervisor", "Workplace Supervisor"),
+        ("academic_supervisor", "Academic Supervisor"),
+        ("internship_admin", "Internship Administrator")
+    ]
+
+    role = models.CharField(choices=ROLES, max_length=30)
+
+    #student_id = models.CharField( unique=True, blank=True, null=True, max_length=20, help_text="25/U/0001")
+
+    phone_number = models.CharField(max_length=20, blank=True)
+    university = models.CharField(max_length=200, blank=True)
+    course = models.CharField(max_length=200, blank=True)
+    department = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.get_role_display()})"
