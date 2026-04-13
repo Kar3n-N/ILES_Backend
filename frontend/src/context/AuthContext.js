8f095dc9adfa8d7bfd7cc9cba2b07755c2d0e208
Sabano Zubedah Angel <sabanozubedah@gmail.com>
2026-04-13 14:52:01 +0300

    Edited Auth context.js

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -78,22 +78,4 @@ export function AuthProvider({children}){
     );
 }
 
-
-import { useContext } from "react";
-import {AuthContext} from "../context/AuthContext";
-
-
-export function useAuth() {
-    const context = useContext(AuthContext);
-
-
-
-    if(context === null) {
-        throw new Error(
-            "useAuth() was called outside of <AuthProvider>. "+
-            "Make sure <AuthProvider> wraps your component in App.js."
-        );
-    }
-
-    return context;
-}
+ 
