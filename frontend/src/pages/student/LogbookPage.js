349268199b19f0944c63e73f69d4e52e132b155d
Matthew Mumbere <matthewmumbere@gmail.com>
2026-05-07 12:31:33 +0300

    feat(ILES-52): scaffolding with imports of Primitives

--- frontend/src/pages/student/LogbookPage.js
+++ frontend/src/pages/student/LogbookPage.js
@@ -1,4 +1,15 @@
 import React from "react";
+import PageShell from "../components/PageShell/PageShell";
+import {
+  PageHead,
+  Card,
+  Btn,
+  Chip,
+  Field,
+  Bar,
+  Lines,
+} from "../components/common/Primitives";
+import { I } from "../components/common/Icons";
 
 function LogbookPage() {
   return <h1> LOGBOOK PAGE</h1>;
