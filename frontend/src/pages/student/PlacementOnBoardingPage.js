fffe69647d25d1e045ab6bfb35d740510b2dec0e
Nakibuuka Karen <k41444105@gmail.com>
2026-05-07 10:00:41 +0300

    [ILES-51]:creating the default function for this file

--- frontend/src/pages/student/PlacementOnBoardingPage.js
+++ frontend/src/pages/student/PlacementOnBoardingPage.js
@@ -2,3 +2,13 @@ import React from 'react';
 import PageShell from '../components/PageShell/PageShell';
 import { PageHead, Card, Btn, Chip, Field, Lines } from '../components/common/Primitives';
 import { I } from '../components/common/Icons';
+
+export default function OnboardingPage() {
+  return (
+    <PageShell role="student">
+      <PageHead
+        crumb="Onboarding · Step 1 of 3"
+        title="Set up your internship placement"
+        sub="You'll unlock your dashboard once your placement is approved."
+        actions={<><Btn kind="ghost" sm>Save draft</Btn><Btn kind="primary" sm>{I.arrow} Submit for approval</Btn></>}
+      />
